bd334e4a1d25d1812c89f3a374453ab7eec2e2ab
batyoaron <111697446+Batyoaron@users.noreply.github.com>
2024-01-29 15:41:30 +0100

    Add files via upload

---
+++ input.py
@@ -0,0 +1,30 @@
+import subprocess
+
+
+print("PixelPlanet.fun timelapse maker")
+print("by: Batyo")
+print("")
+
+a = input("start x,y coordinates: ")
+b = input("end x,y coordinates: ")
+c = input("start time: ")
+d = input("end time: ")
+
+print("image download starting")
+
+cmd_command = [
+    r"historyDownload.exe",
+    "0",
+    a,
+    b,
+    c,
+    d
+]
+
+result = subprocess.run(cmd_command, capture_output=True, text=True)
+if result.returncode == 0:
+    print("Output:")
+    print(result.stdout)
+else:
+    print("Error:")
+    print(result.stderr)

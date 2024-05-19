9a29eac923d9ddd8bfec6f24a8019e74ae1a8679
yesbatyo <111697446+Batyoaron@users.noreply.github.com>
2024-05-19 19:48:00 +0200

    Update input.py

--- timelapse maker/config/input.py
+++ timelapse maker/config/input.py
@@ -4,21 +4,24 @@ print("PixelPlanet.fun timelapse maker")
 print("by: Batyo")
 print("")
 
+if os.path.isfile("speed"):
+    os.remove("speed")
+else:
+    pass
+
 a = input("start x,y coordinates: ")
 b = input("end x,y coordinates: ")
 start_date = input("start time: ")
 end_date = input("end time: ")
+speed = input("Timelapse speed(fps): ")
+
+f = open("speed", "+a")
+f.write(speed)
+f.close()
 
 print("image download starting")
 
-cmd_command = [
-    r"historyDownload.exe",
-    "0",
-    a,
-    b,
-    start_date,
-    end_date
-]
+cmd_command = "historyDownload.exe 0 ", a, b, start_date, end_date 
 
 cmd_string = " ".join(cmd_command)
 

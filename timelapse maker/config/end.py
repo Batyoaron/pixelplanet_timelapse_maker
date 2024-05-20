d150dcba2a2d8e13639f2d76fa791054b9c6e2c1
yesbatyo <111697446+Batyoaron@users.noreply.github.com>
2024-05-20 19:06:37 +0200

    Update end.py

--- timelapse maker/config/end.py
+++ timelapse maker/config/end.py
@@ -1,3 +1,9 @@
+import os
+import shutil
 print(" Timelapse sucessfully created, you can close this window now")
 print(" You can find the timelapse in the config folder !")
+shutil.rmtree('images')
+shutil.rmtree('timelapse')
+os.mkdir("images")
+os.mkdir("timelapse")
 input()

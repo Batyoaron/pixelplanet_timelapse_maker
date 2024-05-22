1bad2b45336de276ca9e6f672839bcd76ae0a951
yesbatyo <111697446+Batyoaron@users.noreply.github.com>
2024-05-22 15:05:12 +0200

    Update main.py

--- timelapse maker/config/main.py
+++ timelapse maker/config/main.py
@@ -3,6 +3,11 @@ import os
 import re
 import time
 
+import os
+
+desktop = os.path.join(os.path.expanduser('~'), 'Desktop')
+
+
 def create_video_from_images(image_folder, output_path, fps):
     image_files = sorted(os.listdir(image_folder))
     image_files = [file for file in image_files if file.endswith('.png')]
@@ -21,11 +26,15 @@ def create_video_from_images(image_folder, output_path, fps):
     height, width, channels = first_image.shape
 
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+
     video_writer = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
+
 
     start_time = time.time()
     total_images = len(image_files)
     
+
+
     for index, image_file in enumerate(image_files):
         image_path = os.path.join(image_folder, image_file)
         image = cv2.imread(image_path)
@@ -36,9 +45,9 @@ def create_video_from_images(image_folder, output_path, fps):
         elapsed_time = time.time() - start_time
         avg_time_per_image = elapsed_time / (index + 1)
         remaining_time = avg_time_per_image * (total_images - index - 1)
-        os.system("cls")
-        print(f"Processing image {index + 1}/{total_images}, estimated remaining time: {int(remaining_time)} seconds", end='\r')
+        print(f"\rProcessing image {index + 1}/{total_images}, estimated remaining time: {int(remaining_time)} seconds", end='')
         
+
     video_writer.release()
     cv2.destroyAllWindows()
 
@@ -56,7 +65,7 @@ except ValueError:
     speed = 30.0
 
 image_folder = "images"
-output_path = 'timelapse.mp4'
+output_path = os.path.join(desktop, 'timelapse.mp4')
 
 create_video_from_images(image_folder, output_path, speed)
 

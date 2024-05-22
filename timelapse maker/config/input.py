51d48d21dc637360a0dab10564d1c5d46a5a77ae
yesbatyo <111697446+Batyoaron@users.noreply.github.com>
2024-05-22 15:05:29 +0200

    Update input.py

--- timelapse maker/config/input.py
+++ timelapse maker/config/input.py
@@ -10,22 +10,32 @@ if os.path.isfile("speed"):
 else:
     pass
 
+if os.path.isfile("date"):
+    os.remove("date")
+else:
+    pass
+
+
+
 a = input("start x,y coordinates: ")
 b = input("end x,y coordinates: ")
-start_date = input("start time: ")
-end_date = input("end time: ")
+startdate = input("start time: ")
+enddate = input("end time: ")
 speed = input("Timelapse speed(fps): ")
+
+date = (" [D] Start date: " + startdate + "    |  End date: " + enddate)
 
 f = open("speed", "+a")
 f.write(speed)
 f.close()
 
+f = open("date", "+a")
+f.write(str(date))
+f.close()
+
 print("image download starting")
-
-cmd_command = "historyDownload.exe 0 ", a, b, start_date, end_date 
-
+cmd_command = "historyDownload.exe 0 ", a, b, startdate, enddate 
 cmd_string = " ".join(cmd_command)
-
 os.system(cmd_string)
 
 

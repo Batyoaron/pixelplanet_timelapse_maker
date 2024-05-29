05a985eadf2b3e09bfd89a09e2aa44cd80c87677
yesbatyo <111697446+Batyoaron@users.noreply.github.com>
2024-05-29 20:40:27 +0200

    Update end.py

--- timelapse maker/config/end.py
+++ timelapse maker/config/end.py
@@ -2,6 +2,7 @@ import os
 import shutil
 print(" Timelapse sucessfully created, you can close this window now")
 print(" You can find the timelapse in the desktop !")
+print(" If you cant find the timelapse video, then use windows searcher, and search for it.")
 shutil.rmtree('images')
 os.mkdir("images")
-input()
+input()s

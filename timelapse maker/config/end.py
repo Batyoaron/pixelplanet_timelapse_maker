8b215ca70781eacc2792802304ec95c3d44c85eb
yesbatyo <111697446+Batyoaron@users.noreply.github.com>
2024-06-04 14:48:06 +0200

    Update end.py

--- timelapse maker/config/end.py
+++ timelapse maker/config/end.py
@@ -1,8 +1,14 @@
 import os
 import shutil
 print(" Timelapse sucessfully created, you can close this window now")
-print(" You can find the timelapse in the desktop !")
+
+if os.path.isfile("outputpath.txt"):
+    f = open("outputpath.txt", "r")
+    a = f.read()
+    print(" You can find the timelapse in ", a ," !")
+else:
+    print(" You can find the timelapse in the desktop !")
 print(" If you cant find the timelapse video, then use windows searcher, and search for it.")
 shutil.rmtree('images')
 os.mkdir("images")
-input()s
+input()

0bb7fbe57be8beeb18128cc27240c328987b87e3
yesbatyo <111697446+Batyoaron@users.noreply.github.com>
2024-07-28 12:53:37 +0200

    Create end.py

---
+++ timelapse maker/config/converter/end.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+print(" Timelapse sucessfully created, you can close this window now")
+
+try:
+    with open("convertercommand", 'r') as file:
+        remover = file.read()
+except FileNotFoundError:
+    print(f"File not found.")
+
+os.remove(remover)
+
+if os.path.isfile("outputpath.txt"):
+    f = open("outputpath.txt", "r")
+    a = f.read()
+    print(" You can find the timelapse in ", a ," !")
+else:
+    print(" You can find the timelapse in the desktop !")
+print(" If you cant find the timelapse video, then use windows searcher, and search for it.")
+os.remove("convertercommand")
+os.remove("convertercommandsecond")
+if os.path.isfile("outputpath.txt"):
+    os.remove("outputpath.txt")
+input()

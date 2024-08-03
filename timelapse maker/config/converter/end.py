ea32ce5711ab980c75f565d7a5d7c462afb5888b
yesbatyo <111697446+Batyoaron@users.noreply.github.com>
2024-08-03 12:38:57 +0200

    Update end.py

--- timelapse maker/config/converter/end.py
+++ timelapse maker/config/converter/end.py
@@ -7,16 +7,20 @@ try:
 except FileNotFoundError:
     print(f"File not found.")
 
-os.remove(remover)
+if os.path.isfile(remover):
+    os.remove(remover)
 
 if os.path.isfile("outputpath.txt"):
     f = open("outputpath.txt", "r")
     a = f.read()
+    f.close()
     print(" You can find the timelapse in ", a ," !")
 else:
     print(" You can find the timelapse in the desktop !")
 print(" If you cant find the timelapse video, then use windows searcher, and search for it.")
 print(" If you can see two videos, delete the one that has NO underscore in the end of its name")
+if os.path.isfile("outputpath.txt"):
+    os.remove("outputpath.txt")
 os.remove("convertercommand")
 os.remove("convertercommandsecond")
 input()

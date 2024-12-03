836138566a403b27b464b8a0b5f636b3e8c2c203
yesbatyo <111697446+Batyoaron@users.noreply.github.com>
2024-12-03 18:33:51 +0100

    Update launcher.py

--- timelapse maker/launcher.py
+++ timelapse maker/launcher.py
@@ -1,3 +1,11 @@
 import os
+
+if os.path.isfile("config/input.exe"):
+    pass
+else:
+    print(" [!] Didnt found any file to open \n\n Possible errors: \n [1]: Antivirus blocked to open the application \n [2]: You only extracted this file, which is not enough")
+    input(" \n enter to quit")
+    exit()
+
 os.chdir("Config")
 os.system("input.exe")
